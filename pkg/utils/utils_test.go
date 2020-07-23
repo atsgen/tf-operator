5b4623eab1928a24ac6a04773b85ebb8c98f9a51
subramanian96 <subramanian@atsgen.com>
2020-07-23 12:36:50 +0000

    Updated testing for utils pkg.
    Signed-off-by: subramanian96 <subramanian@atsgen.com>

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1,54 +1,87 @@
-package utils_test
+package utils
 
 import (
         "testing"
         "os"
         "github.com/atsgen/tf-operator/pkg/values"
-        "github.com/atsgen/tf-operator/pkg/utils"
 )
 
-func TestGetAdminPassword( t *testing.T ) {
-        testPass := utils.GetAdminPassword()
-        if testPass != utils.DefaultAdminPassword {
+// Trying to set a value and check for its functionality 
+// as part of the testing.
+
+//  Verification for Admin password
+func TestAdminPassword( t *testing.T ) {
+        testPass := GetAdminPassword()
+        SetAdminPassword("password")
+        setPass := GetAdminPassword()
+        if ((testPass != DefaultAdminPassword) || (setPass != "password")) {
                 t.Errorf("FAILED")
         }
+        os.Unsetenv(AdminPasswordEnvVar)
 }
 
+// Testing for the return of out of cluster configuration for K8S API
 func TestGetKubernetesAPIServer( t *testing.T ) {
-        os.Setenv("KUBERNETES_SERVICE_HOST",  "test")
-        testHost := utils.GetKubernetesAPIServer()
-        if testHost!="test" { 
+        sampleHost := GetKubernetesAPIServer()
+        os.Setenv(KubernetesServiceHostEnvVar, "test")
+        testHost := GetKubernetesAPIServer()
+        if ((testHost != "test") || (sampleHost != "")) {
                 t.Errorf("FAILED")
-        }  
+        }
+        // Removing the set value
+        os.Unsetenv(KubernetesServiceHostEnvVar)
 }
 
+// Testing for container registry 
 func TestGetContainerRegistry( t *testing.T ) {
-        os.Setenv("CONTAINER_REGISTRY", "test")
-        testRegistry := utils.GetContainerRegistry() 
-        if testRegistry!="test" {
+        defRegistry := GetContainerRegistry()
+        os.Setenv(ContainerRegistryEnvVar, "test")
+        testRegistry := GetContainerRegistry()
+        if ((testRegistry != "test") || (defRegistry != DefaultContainerRegistry)) {
                 t.Errorf("FAILED")
-        }  
+        }
+        os.Unsetenv(ContainerRegistryEnvVar)
 }
 
+// Checking for the container prefix
 func TestGetContainerPrefix( t *testing.T ) {
-        testPrefix := utils.GetContainerPrefix()
-        if testPrefix != utils.ContainerPrefixContrail {
+        defPrefix := GetContainerPrefix()
+        os.Setenv(ContainerPrefixEnvVar, "test")
+        testPrefix := GetContainerPrefix()
+        if ((defPrefix != ContainerPrefixContrail) || (testPrefix != "test"))  {
                 t.Errorf("FAILED")
         }
+        os.Unsetenv(ContainerPrefixEnvVar)
 }
- 
+
+// Testing for the return of release tag, both default and configured
 func TestGetReleaseTag ( t *testing.T ) {
-        testTag :=  utils.GetReleaseTag("auto")
-        if testTag != values.TFCurrentRelease {    
+        testTag := GetReleaseTag("")
+        configTag := GetReleaseTag("test")
+        if ((testTag != values.TFCurrentRelease) || (configTag != "test")) {
                 t.Errorf("FAILED")
         }
 }
 
+// Checking for K8S API port
 func TestGetKubernetesAPIPort( t *testing.T ) {
-        testPort := utils.GetKubernetesAPIPort() 
-        if testPort != "6443" {
+        defPort := GetKubernetesAPIPort()
+        os.Setenv(KubernetesServicePortEnvVar, "1234")
+        testPort := GetKubernetesAPIPort()
+        if ((defPort != "6443") || (testPort != "1234")) {
                 t.Errorf("FAILED")
         }
+        os.Unsetenv(KubernetesServicePortEnvVar)
 }
 
+// Testing for the namespace in which operator is running
+func TestGetOperatorNamespace( t *testing.T ) {
+        sampleNamespace := GetOperatorNamespace()
+        os.Setenv(OperatorNamespaceEnvVar, "test")
+        testNamespace := GetOperatorNamespace()
+        if ((testNamespace != "test") || (sampleNamespace != "")) {
+                t.Errorf("FAILED")
+        }
+        os.Unsetenv(OperatorNamespaceEnvVar)
+}
 
